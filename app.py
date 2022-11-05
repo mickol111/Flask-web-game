cb84bb72ef7dbe9fa424a0a5304bb167bdc9b6fa
mickol111 <michkola1999@gmail.com>
2022-11-05 14:17:12 +0100

    a0.4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from threading import Lock
 from flask import Flask, render_template, session, request, \
     copy_current_request_context
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
-    close_room, rooms, disconnect
+    close_room, rooms, disconnect,send
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
@@ -14,6 +14,12 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
+
+gCount = 0
+gRooms = []
+
+#constants
+ROOM_CAP = 2
 
 
 def background_thread():
@@ -46,13 +52,58 @@ def my_broadcast_event(message):
          broadcast=True)
 
 
-@socketio.event
-def join(message):
-    join_room(message['room'])
+#@socketio.event
+#def join(message):
+#    join_room(message['room'])
+#    session['receive_count'] = session.get('receive_count', 0) + 1
+#    emit('my_response',
+#         {'data': 'In rooms: ' + ', '.join(rooms()),
+#          'count': session['receive_count']})
+
+@socketio.on('join')
+def on_join(data):
+    global gRooms
+    username = data['username']
+    room = data['room']
+    sid = request.sid
+    roomIdx = next((i for i, x in enumerate(gRooms) if x["room"] == room), None)
+    if roomIdx !=None:
+        usersInRoom = len(gRooms[roomIdx].get("users"))
+        if  usersInRoom < ROOM_CAP:
+            join_room(room)
+            session['receive_count'] = session.get('receive_count', 0) + 1
+            print(username + ' has entered the room: ' + room)
+            emit('my_response', {'data': username + ' has entered the room.',
+                                 'count': session['receive_count']},
+                 to=room)
+            print(gRooms)
+            gRooms[roomIdx]["users"].append(username)
+            print(gRooms)
+        else:
+            session['receive_count'] = session.get('receive_count', 0) + 1
+            print(username + ' cannot enter the room: ' + room + '. Users in the room: ' +str(usersInRoom)+'/'+str(ROOM_CAP))
+            emit('my_response', {'data': username + ' cannot enter the room: ' + room + '. Users in the room: ' +str(usersInRoom)+'/'+str(ROOM_CAP),
+                                 'count': session['receive_count']})
+    else:
+        join_room(room)
+        session['receive_count'] = session.get('receive_count', 0) + 1
+        print(room + ' has been created.')
+        print(username + ' has entered the room: ' + room)
+        emit('my_response', {'data': room + ' has been created. '+username + ' has entered the room: ' + room,
+                             'count': session['receive_count']})
+        emit('my_response', {'data': room + ' has been created. '+username + ' has entered the room: ' + room,
+                             'count': session['receive_count']},
+             to=room)
+        print(gRooms)
+        gRooms.append({"room": room, "users": [username]})
+        print(gRooms)
+
+    join_room(room)
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+    print(username + ' has entered the room: ' + room)
+    emit('my_response', {'data': username + ' has entered the room.',
+                         'count': session['receive_count']},
+         to=room)
 
 
 @socketio.event
@@ -86,7 +137,6 @@ def disconnect_request():
     @copy_current_request_context
     def can_disconnect():
         disconnect()
-
     session['receive_count'] = session.get('receive_count', 0) + 1
     # for this emit we use a callback function
     # when the callback function is invoked we know that the message has been
@@ -104,15 +154,24 @@ def my_ping():
 @socketio.event
 def connect():
     global thread
+    global gCount
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(background_thread)
+    gCount += 1
+    print('Client Connected  ' + str(gCount))
     emit('my_response', {'data': 'Connected', 'count': 0})
+    emit('status', {'count': gCount}, broadcast=True)
 
 
 @socketio.on('disconnect')
 def test_disconnect():
+    global gCount
     print('Client disconnected', request.sid)
+    gCount -= 1
+    print("gCount: " + str(gCount))
+    emit('status', {'count': gCount},broadcast=True)
+
 
 
 if __name__ == '__main__':
